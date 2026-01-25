00ac16d9f255efb0f069286c48cc32dbae5e1e69
Ryley Herrington <ryleyherrington@gmail.com>
2026-01-24 16:22:16 -0800

    Initial Commit

---
+++ screenshotter/screenshotterApp.swift
@@ -0,0 +1,17 @@
+//
+//  screenshotterApp.swift
+//  screenshotter
+//
+//  Created by Ryley Herrington on 1/24/26.
+//
+
+import SwiftUI
+
+@main
+struct screenshotterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
